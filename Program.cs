69837ee8114029a96f9a38987961be283c2bac0d
AnaPaulaVeloso <velosoanapaula29@gmail.com>
2021-09-04 16:08:23 -0300

    Concluir a tarefa Cebolinha

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fraseDigitada;
+            Console.Write("Digite uma flase:");
+            fraseDigitada = Console.ReadLine();
+
+            string fraseComDislalia = fraseDigitada
+            .Replace("r", "l")
+            .Replace("R","L");
+        
+
+            Console.WriteLine(fraseComDislalia);
+        }
+    }
+}
